999708635a4bf466eef671d317b9a768d585062e
Ardo <90957355+axdevelopment@users.noreply.github.com>
2025-04-30 12:50:15 +0200

    made it standalone & optimized

--- server.lua
+++ server.lua
@@ -1,104 +1,112 @@
--- do not touch until you know what you're doing-stuff
-ESX = exports["es_extended"]:getSharedObject()
-local time = os.time()
+-- config
+TimeInterval = 60                -- in Minuten
+DistanceToVehicles = 50.0       -- Abstand in Metern
+Command = "delcars"             -- Admin-Kommando
 
--- config-stuff
-TimeInterval = 60 -- time interval in minutes before wiping the vehicles
-DistanceToVehicles = 50.0 -- minimum distance between car and vehicle, for the vehicle not to be deleted
-Command = "delcars" -- admin command to execute the vehicle wipe manually
-
-AllowedGroups = { -- allowed groups
-    "owner",
-	"admin",
-	"dev"
+AllowedGroups = {               -- Identifiers (steam:, license:, etc.)
+    "steam:11000011abcdef12",
+    "license:7c4bcf123456abcdef"
 }
 
-Messages = { -- notify messages (more like warning messages before the vehiclewipe actually gets executed)
-    [1] = { 
-		time = 5, -- time in minutes before executing the vehiclewipe
-        msg = "Cars located more than 50 meters away from a ped will be removed within 5 minutes!",  -- message to show
-    },
-    [2] = {
-        time = 3,
-        msg = "Cars located more than 50 meters away from a ped will be removed within 3 minutes!",
-    },
-    [3] = {
-        time = 1,
-        msg = "Cars located more than 50 meters away from a ped will be removed within 1 minute!",
-    },
-	-- add as many more as you want
+Messages = {
+    [1] = { time = 5, msg = "Cars more than 50 meters away from a ped will be deleted in 5 minutes!" },
+    [2] = { time = 3, msg = "Cars more than 50 meters away from a ped will be deleted in 3 minutes!" },
+    [3] = { time = 1, msg = "Cars more than 50 meters away from a ped will be deleted in 1 minute!" },
 }
 
 NotAllowed = "Your bitchass is not allowed to use this command!"
 DeletedVehs = " vehicles have been deleted!"
 
-function notify(text, source)
-	if not source then
-		TriggerClientEvent('esx:showNotification', -1, text)
-	else
-		TriggerClientEvent('esx:showNotification', source, text)
-	end
+-- notify function
+function notify(msg, source)
+    if source then
+        TriggerClientEvent('chat:addMessage', source, { args = { "[VehicleWipe]", msg } })
+    else
+        TriggerClientEvent('chat:addMessage', -1, { args = { "[VehicleWipe]", msg } })
+    end
 end
 
--- code-stuff
-function gMT(cT)
-	for _, v in pairs(Messages) do
-		if (TimeInterval * 60 - v.time * 60) == (cT - time) then
-			return v.msg
-		end
-	end
-end
+-- internal timer
+local lastWipe = os.time()
 
-function wipeVehicles()
-	local vehicles = GetAllVehicles()
-	local delVhcs = 0
-	for k, v in pairs(vehicles) do
-		if v and DoesEntityExist(v) then
-			local aVN = false
-			for _, ped in pairs(GetAllPeds()) do
-				if #(GetEntityCoords(v) - GetEntityCoords(ped)) <= DistanceToVehicles then
-					aVN = true
-					break
-				end
-			end
-			if not aVN then
-				DeleteEntity(v)
-				delVhcs = delVhcs + 1
-			end
-		end
-	end
-	notify(delVhcs .. DeletedVehs)
-end
-
-CreateThread(function()
-	while true do
-		Wait(1000)
-		local cT = os.time()
-		if cT - time >= TimeInterval * 60 then
-			time = cT
-			wipeVehicles()
-		else
-			local msg = gMT(cT)
-			if msg then
-				notify(msg)
-			end
-		end
-	end
-end)
-
-RegisterCommand(Command, function(source, args, rawCommand)
-	local xPlayer = ESX.GetPlayerFromId(source)
-    local playerGroup = xPlayer.getGroup()
-    local hasPermissionAndABigDick = false
-    for _, v in pairs(AllowedGroups) do
-        if playerGroup == v then
-            hasPermissionAndABigDick = true
-            break
+-- message timing logic
+function getWarningMessage(currentTime)
+    for _, v in pairs(Messages) do
+        if (TimeInterval * 60 - v.time * 60) == (currentTime - lastWipe) then
+            return v.msg
         end
     end
-    if hasPermissionAndABigDick then
+end
+
+-- vehicle wipe
+function wipeVehicles()
+    local deleted = 0
+    local allPeds = GetAllPeds()
+    local allVehicles = GetAllVehicles()
+
+    for _, veh in pairs(allVehicles) do
+        if DoesEntityExist(veh) then
+            local vehCoords = GetEntityCoords(veh)
+            local tooClose = false
+
+            for _, ped in pairs(allPeds) do
+                if #(vehCoords - GetEntityCoords(ped)) <= DistanceToVehicles then
+                    tooClose = true
+                    break
+                end
+            end
+
+            if not tooClose then
+                DeleteEntity(veh)
+                deleted = deleted + 1
+            end
+        end
+    end
+
+    notify(deleted .. DeletedVehs)
+end
+
+-- main timer loop
+CreateThread(function()
+    while true do
+        Wait(1000)
+        local now = os.time()
+
+        if now - lastWipe >= TimeInterval * 60 then
+            lastWipe = now
+            wipeVehicles()
+        else
+            local msg = getWarningMessage(now)
+            if msg then
+                notify(msg)
+            end
+        end
+    end
+end)
+
+-- manual command
+RegisterCommand(Command, function(source)
+    if source == 0 then
+        wipeVehicles()
+        return
+    end
+
+    local ids = GetPlayerIdentifiers(source)
+    local isAllowed = false
+
+    for _, id in pairs(ids) do
+        for _, allowed in pairs(AllowedGroups) do
+            if string.lower(id) == string.lower(allowed) then
+                isAllowed = true
+                break
+            end
+        end
+        if isAllowed then break end
+    end
+
+    if isAllowed then
         wipeVehicles()
     else
-		notify(NotAllowed, source)
+        notify(NotAllowed, source)
     end
 end, false)
